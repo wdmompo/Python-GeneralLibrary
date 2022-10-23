12ba57e46a0b736d661552e435914d72aac95bf2
Walter Damian Mompo <wdmompo@gmail.com>
2022-10-22 23:31:48 -0300

    Creación clase de cuil-cuit

--- cuil-cuit.py
+++ cuil-cuit.py
@@ -1,3 +1,9 @@
-import sys
+class CuilCuit:
+    __id = 0
+    __numero = 0
+    __dv = 0
 
-print(sys.argv)
+    def __init__(self, id, numero, dv):
+        self.__id = id
+        self.__numero = numero
+        self.__dv = dv
